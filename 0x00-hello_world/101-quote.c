430ef96a9b7bc0c4ff1e68d221f15ed5b1bff992
Ndondo-Josue <ndondojosue@gmail.com>
2020-09-18 18:59:31 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,21 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
  
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */ 
-int main (void)
+int main(void)
 {
 int w;
-/* faire l’affichage avec write (2) ici */
-w = write (STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10); 
-if (w < 0)
-perror ("Writing error: ");
+w = write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10); 
+if(w < 0)
+  {
+    perror("Writing error: ");
+  }
 return(1);
 }
